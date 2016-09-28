456b515c449e72d075a2a93bab83a048f96be27f
zhousaito <zhousaito@163.com>
2016-09-28 08:45:36 +0800

    fragment

---
+++ app/src/main/java/qf/com/news/mvpview/fragment/BaseFragment.java
@@ -0,0 +1,11 @@
+package qf.com.news.mvpview.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by zhousaito on 2016/9/28.
+ */
+
+public class BaseFragment extends Fragment{
+
+}
